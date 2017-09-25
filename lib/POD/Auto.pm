f3ba3c66fa33c7c7263dfb9a9440c9795d0a33ef
Cesar Tessarin <cesar.tessarin@gmail.com>
2017-09-25 19:05:13 -0300

    Implement basic functionality for long options

--- lib/POD/Auto.pm
+++ lib/POD/Auto.pm
@@ -6,4 +6,21 @@ use warnings;
 
 our $VERSION = '1.0';
 
+sub import {
+    my $arg = $_[1] // 'help';
+    search_for($arg);
+}
+
+sub search_for {
+    my $arg = shift;
+    foreach (@ARGV) {
+	show_docs() if $_ eq "--$arg";
+    }
+}
+
+sub show_docs {
+    @ARGV = ('-F', $0);
+    exit Pod::Perldoc->run();
+}
+
 1;
