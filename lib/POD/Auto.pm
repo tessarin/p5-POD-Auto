4bd3fc0f8b0ad4b5689335c39ccc95a91833acf9
Cesar Tessarin <cesar.tessarin@gmail.com>
2017-09-29 18:05:58 -0300

    Improve argument parsing and flag support
    The argument given at `use` will now be parsed and scanned for both long
    and short options separated by `|`, similar to GetOpt arguments. Only
    one of each may be used at once.

--- lib/POD/Auto.pm
+++ lib/POD/Auto.pm
@@ -4,6 +4,9 @@ use 5.006;
 use strict;
 use warnings;
 
+use Carp;
+use Pod::Perldoc;
+
 our $VERSION = '1.0';
 
 sub import {
@@ -13,9 +16,34 @@ sub import {
 
 sub search_for {
     my $arg = shift;
+    my ($long, $short) = process_arguments($arg);
     foreach (@ARGV) {
-	show_docs() if $_ eq "--$arg";
+	if ( ( $long && $_ eq "--$long" ) ||
+	     ( $short && /^-\w*$short/  ) ) {
+	    show_docs();
+	}
     }
+}
+
+sub process_arguments {
+    return () unless $_[0];
+    my $arg = shift;
+    my @pieces = split /\|/, $arg;
+    croak 'too many flags to detect' if @pieces > 2;
+
+    my ($long, $short);
+    foreach (@pieces) {
+	if (length == 1) {
+	    croak 'only one short flag allowed' if $short;
+	    $short = $_;
+	}
+	if (length > 1) {
+	    croak 'only one long flag allowed' if $long;
+	    $long = $_;
+	}
+    }
+
+    return ($long, $short);
 }
 
 sub show_docs {
